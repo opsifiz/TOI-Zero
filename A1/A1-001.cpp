5e3bb27cf0d2dc88973040958ddb8b0f265711a9
MisterO <119954673+opsifiz@users.noreply.github.com>
2025-05-16 01:19:47 +0700

    Create A1-001.cpp

---
+++ A1/A1-001.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+int main(){
+	string fname, lname;
+	cin>>fname>>lname;
+	cout<<"Hello "<<fname<<" "<<lname<<endl;
+	cout<<fname[0]<<fname[1]<<lname[0]<<lname[1];
+}
+/*
+AC : Accept
+WA : Wrong Answer
+TLE : Time Limit Exceeded
+MLE : Memory Limit Exceeded
+RTE, RE : Runtime Error
+*/
